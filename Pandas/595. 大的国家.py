0d77693851075ec1c91d11250cbdd49b18c4e925
Wilson <lwx_324@hotmail.com>
2023-08-01 22:47:48 +0800

    pandas update 思路

--- Pandas/595. 大的国家.py
+++ Pandas/595. 大的国家.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# 方法：行过滤
+# 利用dataframe["列属性"]：条件语句 来筛选适合的行数据
+
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     df = world[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
     return df[["name", "population", "area"]]
+
